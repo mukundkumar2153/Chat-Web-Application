468ab75b695e20baef74e3ca8a8969541387fa35
MUKUND KUMAR <mukundkumar2153@gmail.com>
2026-06-23 07:54:08 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,14 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
+  server: {
+    host: '0.0.0.0'
+  },
+
+  preview: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? Number(process.env.PORT) : 4173,
+    allowedHosts: true
+  }
 })
